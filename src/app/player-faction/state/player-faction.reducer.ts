5e32aa1248ddc1bc3ddcbecf6f24b7d2b5926347
Zandre Botha <botha.zandre@gmail.com>
2022-02-24 13:55:03 +0100

    Completing success parts of reducer

--- src/app/player-faction/state/player-faction.reducer.ts
+++ src/app/player-faction/state/player-faction.reducer.ts
@@ -1,5 +1,6 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
 import { PlayerFactionInterface } from "../interfaces/player-faction.interface";
+import { PlayerFactionPageActions, PlayerFactionApiActions } from "./actions";
 
 export interface PlayerFactionState {
     playerFactions: PlayerFactionInterface[];
@@ -9,6 +10,36 @@ const initialState: PlayerFactionState = {
     playerFactions: []
 }
 
+// TODO: implement error handling
+
 export const playerFactionReducer = createReducer<PlayerFactionState>(
-    initialState
+    initialState,
+    on(PlayerFactionApiActions.loadSuccess, (state, action): PlayerFactionState => {
+        return {
+            ...state,
+            playerFactions: action.playerFactions
+        };
+    }),
+    on(PlayerFactionApiActions.createSuccess, (state, action): PlayerFactionState => {
+        return {
+            ...state,
+            playerFactions: [...state.playerFactions, action.playerFaction]
+        };
+    }),
+    on(PlayerFactionApiActions.updateSuccess, (state, action): PlayerFactionState => {
+        const updatedPlayerFactions = state.playerFactions.map(
+            untouchedFaction => action.playerFaction.id === untouchedFaction.id ? 
+                action.playerFaction : 
+                untouchedFaction)
+        return {
+            ...state,
+            playerFactions: updatedPlayerFactions
+        };
+    }),
+    on(PlayerFactionApiActions.deleteSuccess, (state, action): PlayerFactionState => {
+        return {
+            ...state,
+            playerFactions: state.playerFactions.filter(faction => faction.id !== action.playerFactionId)
+        };
+    })
 )
