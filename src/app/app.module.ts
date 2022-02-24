f0f59e37c165a3b782d2811e94a3df79684df277
Zandre Botha <botha.zandre@gmail.com>
2022-02-24 09:46:02 +0100

    Setting up InMemoryWebApi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
+// Angular
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// NgRX
+import { StoreModule } from '@ngrx/store';
+
+// App modules and components
+import { AppComponent } from './app.component';
 import { ToolbarModule } from './toolbar/toolbar.module';
 import { PlayerFactionModule } from './player-faction/player-faction.module';
-import { StoreModule } from '@ngrx/store';
+
+// Database
+import { PlayerFactionData } from './player-faction/database/player-faction.data';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // Angular
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(PlayerFactionData),
+
+    // NgRX
+    StoreModule.forRoot({}, {}),
+
+    // App modules and components
     ToolbarModule,
     PlayerFactionModule,
-    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/player-faction/database/player-faction.data.ts
@@ -0,0 +1,5 @@
+// import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+// export class PlayerFactionData implements InMemoryDbService {
+
+// }
