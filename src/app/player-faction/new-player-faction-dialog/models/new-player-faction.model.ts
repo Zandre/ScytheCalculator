6c85cf19387269bffd4ea2fba8989a3697a252d9
Zandre Botha <botha.zandre@gmail.com>
2022-02-25 10:09:02 +0100

    Player model & rxweb forms installation

---
+++ src/app/player-faction/new-player-faction-dialog/models/new-player-faction.model.ts
@@ -0,0 +1,63 @@
+import { maxNumber, prop, required } from "@rxweb/reactive-forms";
+import { PlayerFactionType } from "../../enums/player-faction-type.enum";
+import { PlayerFaction } from "../../interfaces/player-faction.interface";
+
+export class NewPlayerFactionModel implements PlayerFaction {
+    
+    @prop()
+    id: number;
+
+    @prop()
+    @required()
+    playerFactionType: PlayerFactionType;
+
+    @prop()
+    @required()
+    @maxNumber({ value: 18 })
+    popularity: number;
+
+    @prop()
+    @required()
+    @maxNumber({ value: 6 })
+    victoryStars: number;
+
+    @prop()
+    @required()
+    @maxNumber({ value: 30 })
+    territories: number;
+
+    @prop()
+    @required()
+    @maxNumber({ value: 50 })
+    resources: number;
+
+    @prop()
+    @required()
+    @maxNumber({ value: 50 })
+    money: number;
+
+    @prop()
+    @required()
+    @maxNumber({ value: 20 })
+    structureBonuses: number;
+
+    static createFromInput(input: PlayerFaction): NewPlayerFactionModel {
+        const model = new NewPlayerFactionModel();
+
+        model.id = input.id;
+        model.playerFactionType = input.playerFactionType;
+        model.popularity = input.popularity;
+        model.victoryStars = input.victoryStars;
+        model.territories = input.territories;
+        model.resources = input.resources;
+        model.money = input.money;
+        model.structureBonuses = input.structureBonuses;
+        
+        return model;
+    }
+
+    static createEmpty(): NewPlayerFactionModel {
+        return new NewPlayerFactionModel();
+    }
+
+}
