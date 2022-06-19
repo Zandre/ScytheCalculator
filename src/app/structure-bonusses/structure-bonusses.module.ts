5fc63b8558c610f7a66caf1f1b550718535ecf78
Zandre Botha <botha.zandre@gmail.com>
2022-06-19 08:21:20 +0200

    some module reordering

--- src/app/structure-bonusses/structure-bonusses.module.ts
+++ src/app/structure-bonusses/structure-bonusses.module.ts
@@ -1,37 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StructureBonusDialogComponent } from './structure-bonus-dialog/structure-bonus-dialog.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { structureBonusReducer } from './state/sructure-bonus.reducer';
 import { StructureBonusEffects } from './state/structure-bonus.effects';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StructureBonusDialogModule } from './structure-bonus-dialog/structure-bonus-dialog.module.component';
 
 @NgModule({
-  declarations: [
-    StructureBonusDialogComponent
-  ],
-  exports: [
-    StructureBonusDialogComponent
-  ],
   imports: [
     CommonModule,
     StoreModule.forFeature('structureBonusType', structureBonusReducer),
     EffectsModule.forFeature([StructureBonusEffects]),
-    MatButtonModule,
-    MatDialogModule,
-    MatIconModule,
-    MatSelectModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RxReactiveFormsModule,
-    FlexLayoutModule
+    StructureBonusDialogModule
   ]
 })
 export class StructureBonussesModule { }
